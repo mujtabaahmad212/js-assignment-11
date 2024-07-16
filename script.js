acd431d645628da83d2137e197571716613ad4a9
Mujtaba Ahmad <140813692+mujtabaahmad212@users.noreply.github.com>
2024-07-16 15:33:07 +0500

    five exercises of js

---
+++ script.js
@@ -0,0 +1,36 @@
+// exercise 1 
+// Task: Declare a variable str and assign it the value "Welcome to JavaScript!". Print the length of the string.
+
+var khan = "Welcome to JavaScript!";
+
+console.log(khan.length);
+
+// exercise 2
+// Task: Declare a variable str and assign it the value "Programming is awesome". Extract and print the word "awesome" from the string.
+
+var programing = "Programming is awesome";
+ console.log(programing.slice(14));
+
+
+ // exercise 3
+//  Task: Declare a variable str and assign it the value "Hello, Universe!". Extract and print the first five characters of the string.
+
+var hello = "Hello, Universe!";
+console.log(hello.slice(0,5));
+
+
+ // exercise 4
+// Task: Declare a variable str and assign it the value "Full Stack Developer". Extract and print the substring "Stack".
+
+var stack = "Full Stack Developer";
+console.log(stack.slice(5,11));
+
+ // exercise 5
+// Task: Declare a variable str and assign it the value "Javascript Strings" Extract and print the last four characters of the string
+
+var extract = "Javascript Strings";
+console.log(extract.slice(14));
+
+
+
+
